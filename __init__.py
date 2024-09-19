2247a4b2c441460e42f3d0def3dc778b90d31dda
Aidan Allchin <aidan@allchin.com>
2024-09-19 19:45:43 -0400

    Started SpotifyAPI work

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+"""
+This script is used to download the VGGish model files and create the necessary directories.
+"""
 
-import os, sys, subprocess
+import os, sys, subprocess, json
 
 # Install the required packages
 subprocess.run(["pip", "install", "-r", "requirements.txt"])
@@ -21,5 +25,21 @@ elif not os.path.exists("data/embeddings"):
 # Download the VGGish model files (WIP)
 subprocess.run(["wget", "https://storage.googleapis.com/audioset/vggish_model.ckpt", "-O", "data/vggish_model/vggish_model.ckpt"])
 subprocess.run(["wget", "https://storage.googleapis.com/audioset/vggish_pca_params.npz", "-O", "data/vggish_model/vggish_pca_params.npz"])
+
+# Set up SpotifyAPI
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config', 'config.json')
+with open(CONFIG_PATH, 'r') as f:
+    config = json.load(f)
+if config['spotify']['client_id'] == "" or config['spotify']['client_secret'] == "":
+    print("Please set up your SpotifyAPI credentials:")
+    client_id = input("Client ID: ")
+    client_secret = input("Client Secret: ")
+    config['spotify']['client_id'] = client_id
+    config['spotify']['client_secret'] = client_secret
+    with open(CONFIG_PATH, 'w') as f:
+        json.dump(config, f, indent=4)
+    print("SpotifyAPI credentials saved.")
+else:
+    print("SpotifyAPI credentials already set up.")
 
 print("Setup complete.")
--- src/embeddings/vgg_maxpool.py
+++ src/embeddings/vgg_maxpool.py
@@ -14,10 +14,10 @@ from pathlib import Path
 project_root = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, str(project_root))
 
+from colorama import Fore, Style
 import json
 import time
 import numpy as np
-from colorama import Fore, Style
 import logging
 import tensorflow.compat.v1 as tf # type: ignore
 from src.embeddings.vgg import vggish_input
