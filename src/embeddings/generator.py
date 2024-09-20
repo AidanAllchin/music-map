653c1dc5b833dc0c4c1fa4d1eaacdb5e939ddcde
AidanAllchin <aidan@allchin.com>
2024-09-20 12:58:25 -0400

    Full process built - not necessarily functional yet. Have to ensure the embeddings format and ytdlp retrieval

--- src/embeddings/generator.py
+++ src/embeddings/generator.py
@@ -12,7 +12,83 @@ project_root = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, str(project_root))
 
 from colorama import Fore, Style
-from src.utils.ytdl_handler import find_best_length, download_one_by_url
+from src.utils.ytdl_handler import download_best
+from src.embeddings.vgg_maxpool import extract_one_embedding
+import pandas as pd
+from tqdm import tqdm
+
+def embed_row(row: pd.Series) -> list:
+    """
+    Embeds a row of the dataframe.
+
+    This function takes a row of the dataframe as input, downloads the song, generates embeddings,
+    and returns the embeddings.
+
+    Args:
+        row (pd.Series): A row of the dataframe.
+
+    Returns:
+        list: The embedding of the row [list of 128 floats].
+    """
+    # Download the song
+    success, file_path = download_best(row['title'], row['duration'])
+
+    if not success:
+        print(f"{Style.BRIGHT}[EmbeddingsGenerator]: {Style.NORMAL}{Fore.RED}Error: Could not download {row['title']}.{Style.RESET_ALL}")
+        return None
+
+    # Generate the embeddings
+    embedding = extract_one_embedding(file=file_path)
+
+    return embedding
 
 def add_embeddings_to_tsv(tsv_path: str):
-    pass
+    """
+    Adds embeddings to a TSV file.
+
+    This function reads a TSV file, generates embeddings for each row, and adds these embeddings
+    as a new column to the dataframe. The updated dataframe is then saved back to the TSV file.
+
+    Args:
+        tsv_path (str): The path to the TSV file.
+
+    Raises:
+        SystemExit: If the file does not exist or if the 'embeddings' column already exists in the dataframe.
+
+    Notes:
+        - The function checks if the file exists at the given path. If not, it prints an error message and exits.
+        - It reads the TSV file into a pandas dataframe.
+        - It ensures that the 'embeddings' column is not already present in the dataframe.
+        - It iterates over each row of the dataframe, generating embeddings and appending them to a list.
+        - The embeddings are added as a new column to the dataframe.
+        - The updated dataframe is saved back to the TSV file.
+        - A success message is printed upon completion.
+    """
+    # Check if the file exists
+    if not os.path.exists(tsv_path):
+        print(f"{Style.BRIGHT}[EmbeddingsGenerator]: {Style.NORMAL}{Fore.RED}Error: File not found.{Style.RESET_ALL}")
+        sys.exit(1)
+
+    # Read
+    df = pd.read_csv(tsv_path, sep='\t')
+
+    # Ensure 'embeddings' column is not already present
+    if 'embeddings' in df.columns:
+        print(f"{Style.BRIGHT}[EmbeddingsGenerator]: {Style.NORMAL}{Fore.RED}Error: 'embeddings' column already exists in the dataframe.{Style.RESET_ALL}")
+        sys.exit(1)
+
+    embeddings = []
+
+    for _, row in tqdm(df.iterrows(), desc="Embedding rows", total=df.shape[0]):
+        embeddings.append(embed_row(row))
+
+    # Add the embeddings to the dataframe
+    df['embeddings'] = embeddings
+
+    # Save the dataframe
+    df.to_csv(tsv_path, sep='\t', index=False)
+
+    print(f"{Style.BRIGHT}[EmbeddingsGenerator]: {Style.NORMAL}{Fore.GREEN}Embeddings added to {tsv_path}.{Style.RESET_ALL}")
+
+if __name__ == "__main__":
+    add_embeddings_to_tsv(sys.argv[1])
--- src/utils/ytdl_handler.py
+++ src/utils/ytdl_handler.py
@@ -13,8 +13,11 @@ project_root = Path(__file__).resolve().parent
 sys.path.insert(0, str(project_root))
 
 from colorama import Fore, Style
+from typing import Tuple
 import yt_dlp
 import contextlib
+
+DESTINATION_PATH_WAVS = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'waveforms')
 
 def find_best_length(search_query, target_length, threshold) -> str:
     ydl_opts = {
@@ -61,6 +64,7 @@ def find_best_length(search_query, target_length, threshold) -> str:
         return None
     
 def download_one_by_url(url):
+    id = url.split('=')[-1]
     def download_youtube_audio(search_for: str):
         #subprocess.run(['sudo', 'rm', '-rf', '~/.cache/yt-dlp'], check=True)
 
@@ -74,7 +78,7 @@ def download_one_by_url(url):
             'cachedir': False,
             'quiet': True,
             # Save the file as to_embed.mp3
-            'outtmpl': os.path.join(os.path.dirname(__file__), '..', 'data', 'spotify', 'to_embed')
+            'outtmpl': os.path.join(DESTINATION_PATH_WAVS, f"yt_id_{id}.mp3")
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -98,3 +102,23 @@ def download_one_by_url(url):
         download_youtube_audio(url)
     sys.stdout = sys.__stdout__
     
+def download_best(search_query: str, target_length: float, threshold: int) -> Tuple[bool, str]:
+    """
+    Downloads the best matching video based on the search query and target length.
+
+    Args:
+        search_query (str): The search query to find the video.
+        target_length (float): The target length of the video in seconds.
+        threshold (int): The acceptable deviation from the target length in seconds.
+
+    Returns:
+        tuple: A tuple containing:
+            - bool: True if a video was found and downloaded, False otherwise.
+            - str: The path to the downloaded video.
+    """
+    url = find_best_length(search_query, target_length, threshold)
+    if url is not None:
+        download_one_by_url(url)
+        return True, os.path.join(DESTINATION_PATH_WAVS, f"yt_id_{url.split('=')[-1]}.mp3")
+    else:
+        return False, None
